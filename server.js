729b002d37d4c3149696d53100d60933eae705e2
Anwesha-P <anwesha.pradhananga@gmail.com>
2023-11-09 14:08:39 -0500

    change

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ function updateUser(req, res, next) {
 }
 
 function createUser(req, res, next) {
-    db.one('INSERT INTO Users(ID, name, emailAddress, username, passwordHash) VALUES ( ${ID}, ${name}, ${email}, $(username), $(passwordHash)) RETURNING id', req.body)
+    db.one('INSERT INTO Users(ID, name, emailAddress, username, passwordHash) VALUES ( ${ID}, ${name}, ${emailAddress}, $(username), $(passwordHash)) RETURNING id', req.body)
         .then(data => {
             res.send(data);
         })
