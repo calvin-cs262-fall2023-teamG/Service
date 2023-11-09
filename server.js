f8f7e3ba6d6e37837bf5da30a195473ce961b550
Anwesha-P <anwesha.pradhananga@gmail.com>
2023-11-09 13:48:37 -0500

    changes

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ function deleteUser(req, res, next) {
 }
 
 function readBooks(req, res, next) {
-    db.many("SELECT * FROM Books")
+    db.many("SELECT b.ID, b.title, b.author, b.isbn, b.price, b.courseName, b.date_sold, b.userID, b.front_picture, b.back_picture, u.name, u.emailAddress FROM Books b, Users u  WHERE u.ID = b.userID")
         .then(data => {
             res.send(data);
         })
@@ -135,7 +135,7 @@ function readBooks(req, res, next) {
 }
 
 function readBook(req, res, next) {
-    db.oneOrNone('SELECT * FROM Books WHERE id=${id}', req.params)
+    db.oneOrNone('SELECT b.ID, b.title, b.author, b.isbn, b.price, b.courseName, b.date_sold, b.userID, b.front_picture, b.back_picture, u.name, u.emailAddress FROM Books b, Users u  WHERE u.ID = b.userID and b.id=${id}', req.params)
         .then(data => {
             returnDataOr404(res, data);
         })
