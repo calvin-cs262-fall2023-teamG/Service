785d29197eed667c8c6d18179e6c318f02a8d8d9
Anwesha-P <anwesha.pradhananga@gmail.com>
2023-11-25 11:10:29 -0500

    changed update book

--- server.js
+++ server.js
@@ -148,7 +148,7 @@ function readBook(req, res, next) {
 }
 
 function updateBook(req, res, next) {
-  db.oneOrNone('UPDATE Books SET ID = ${id}, title = ${title}, author = ${author}, isbn = ${isbn}, coursename = ${coursename}, userID = ${userID}, price = ${price}) WHERE id=${params.id} RETURNING id', req.body)
+  db.oneOrNone('UPDATE Books SET ID = ${id}, title = ${title}, author = ${author}, isbn = ${isbn}, coursename = ${coursename}, userID = ${userID}, price = ${price}) WHERE ID=${id} RETURNING id', req.body)
     .then((data) => {
       returnDataOr404(res, data);
     })
